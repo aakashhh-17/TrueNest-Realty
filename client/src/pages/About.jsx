3e96a3baf9709de34418d1b5db09d7d6842c2cd6
aakashhh-17 <aakashbhujbal1704@gmail.com>
2025-06-09 18:15:07 +0530

    Completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='p-25'>
+      <h1 className='text-4xl font-bold text-slate-700 mb-5'>About TrueNest Realty</h1>
+      <div className=' flex flex-col gap-3'>
+      <p>At TrueNest Realty, we believe that every property tells a story—and we’re here to help you find the one that feels like home. Whether you're searching for your dream house, investing in real estate, or listing a property, TrueNest Realty is your trusted partner in making confident and informed decisions.</p>
+      <p>Founded with a passion for integrity, transparency, and exceptional service, we combine local expertise with a personalized approach. Our platform offers curated listings, smart filters, and up-to-date market insights, making the property search seamless and efficient.</p>
+      <p>From modern apartments and cozy rentals to luxury estates and commercial spaces, TrueNest Realty is committed to matching you with spaces that suit your lifestyle and needs. Because at the end of the day, it's not just about real estate—it's about helping you find your true nest.</p>
+      </div>
+    </div>
   )
 }
